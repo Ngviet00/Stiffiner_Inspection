7cd083cb7b1976d8180a44c49b545754db425eca
ngviet00 <ngvviet0306@gmail.com>
2024-01-19 11:21:44 +0700

    init project

---
+++ Stiffiner_Inspection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stiffiner_Inspection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
