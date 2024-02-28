8690cd046c2576241d7de4e30421db0044122423
ngviet00 <ngvviet0306@gmail.com>
2024-02-28 18:08:56 +0700

    develop

---
+++ Stiffiner_Inspection/wwwroot/js/get-current-time.js
@@ -0,0 +1,28 @@
+function updateCurrentTime() {
+    let currentDate = new Date();
+    let _currentDate = document.getElementById("date");
+    let currentTime = document.getElementById("current-time");
+
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+    };
+
+    //set current date
+    _currentDate.innerHTML = currentDate.toLocaleString('en-US', options);
+
+    currentDate = new Date();
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    const formatTime = `${hours}:${minutes}:${seconds}`;
+
+    //set current time
+    currentTime.innerHTML = formatTime;
+}
+
+updateCurrentTime();
+
+setInterval(updateCurrentTime, 1000);
