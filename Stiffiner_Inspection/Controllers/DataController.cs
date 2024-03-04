82c1f297315989561b7f5cfcca5717383b9dbb16
ngviet00 <ngvviet0306@gmail.com>
2024-03-04 10:50:04 +0700

    feat/add-api-data

---
+++ Stiffiner_Inspection/Controllers/DataController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Stiffiner_Inspection.Controllers
+{
+    [ApiController]
+    public class DataController : Controller
+    {
+        [Route("api/data/get-data")]
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            var dataList = new List<object>
+            {
+                new {
+                    id = 1,
+                    status = 200,
+                    title = "test",
+                    result = "OK",
+                    error_code = "100",
+                    client_id = 1,
+                    time = "13:10:00",
+                },
+                new {
+                    id = 2,
+                    status = 200,
+                    title = "test",
+                    result = "OK",
+                    error_code = "100",
+                    client_id = 1,
+                    time = "13:10:00",
+                },
+            };
+
+            return Ok(dataList);
+        }
+
+        [Route("api/data/post-data")]
+        [HttpPost]
+        public IActionResult PostData()
+        {
+            var data = new
+            {
+                status = 200,
+                message = "success"
+            };
+
+            return Ok(data);
+        }
+    }
+}
