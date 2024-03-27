f6d11e40e0b6bcdb713f4dc8b8fdc2bb2d967f4a
ngviet00 <ngvviet0306@gmail.com>
2024-03-27 18:04:15 +0700

    dev

--- Stiffiner_Inspection/Services/DataService.cs
+++ Stiffiner_Inspection/Services/DataService.cs
@@ -377,7 +377,7 @@ namespace Stiffiner_Inspection.Services
 
             if (total >= 40)
             {
-                maxTray++;
+                currTray = maxTray++;
             }
             else
             {
